ba3392925518248953d33744f8d3354b596483b0
Gerardo Ravago <gerardo@gcr.me>
2019-02-23 17:36:07 -0500

    Create SAM blinky app

---
+++ sam-app/blinky/app.py
@@ -0,0 +1,18 @@
+import boto3
+import os
+
+def lambda_handler(event, context):
+  print('Started blink handler.')
+
+  topic = os.environ['IOT_TOPIC']
+  print('Publishing to topic: ' + topic)
+
+  iot = boto3.client('iot-data')
+  response = iot.publish(topic=topic)
+  print(response)
+  if response['ResponseMetadata']['HTTPStatusCode'] == 200:
+    print('HTTP 200: Success')
+    return {'statusCode': 200}
+  else:
+    print('Failed response.')
+    return {'statusCode': 500}
